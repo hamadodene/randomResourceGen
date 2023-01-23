f1a11ba3788af08682ffd0a2432dd1e219e9b203
hamadodene <hamadodene@yahoo.com>
2023-01-23 17:43:41 +0100

    Fix

--- app/client.py
+++ app/client.py
@@ -7,8 +7,17 @@ import config
 import random
 import string
 
-url = config.REVERSE_PROXY_URL
+URL = config.REVERSE_PROXY_URL
 num_requests = config.NUM_CLIENT_REQUEST
+ssl_verification = True if config.SSL_VERIFICATION.lower == "true" else False
+
+# Create a session with no SSL verification
+session = requests.Session()
+session.verify = ssl_verification
+
+# Function to make a request using the session
+def make_request(url):
+    return session.get(url)
 
 start_time = time.time()
 
@@ -18,14 +27,14 @@ def random_string(string_lenght=10):
     return ''.join(random.choice(letters) for i in range(string_lenght))
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = [executor.submit(requests.get, url + '/imagergb' + random_string() + '.png') for i in range(num_requests)]
-    #results = [executor.submit(requests.get, url + '/image' + random_string() + '.png') for i in range(num_requests)]]
+    results = [executor.submit(make_request, URL + '/imagergb/' + random_string() + '.png') for i in range(num_requests)]
+    #results = [executor.submit(requests.get, url + '/image/' + random_string() + '.png') for i in range(num_requests)]]
     for future in concurrent.futures.as_completed(results):
         start_time_req = time.time()
         # Get the response from the Future object
         response = future.result()
         end_time_req = time.time()
-        print(f"Request {response.request.url} returned status code {response.status_code} in {end_time_req - start_time_req} seconds")
+        print(f"Request {response.url} returned status code {response.status_code} in {end_time_req - start_time_req} seconds")
 
 end_time = time.time()
 
--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ NUM_WORKERS=4
 # Client config
 REVERSE_PROXY_URL = "http://localhost"
 NUM_CLIENT_REQUEST = 1000
+SSL_VERIFICATION = "False"
